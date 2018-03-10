5199db13cde450c16a288a6e410039c163c0e7d6
DarrenRatcliffe <dratcliffe17@googlemail.com>
2018-03-10 14:32:01 +0000

    Update os-type.sh

--- scripts/os-type.sh
+++ scripts/os-type.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 
-get_distribution_type()
-{
-    local dtype
-    # Assume unknown
-    dtype="unknown"
+arch=$(uname -m)
+kernel=$(uname -r)
+if [ -n "$(command -v lsb_release)" ]; then
+	distroname=$(lsb_release -s -d)
+elif [ -f "/etc/os-release" ]; then
+	distroname=$(grep PRETTY_NAME /etc/os-release | sed 's/PRETTY_NAME=//g' | tr -d '="')
+elif [ -f "/etc/debian_version" ]; then
+	distroname="Debian $(cat /etc/debian_version)"
+elif [ -f "/etc/redhat-release" ]; then
+	distroname=$(cat /etc/redhat-release)
+else
+	distroname="$(uname -s) $(uname -r)"
+fi
 
-    # First test against Fedora / RHEL / CentOS / generic Redhat derivative
-    if [ -r /etc/rc.d/init.d/functions ]; then
-        source /etc/rc.d/init.d/functions
-        [ zz`type -t passed 2>/dev/null` == "zzfunction" ] && dtype="redhat"
-
-    # Then test against SUSE (must be after Redhat,
-    # I've seen rc.status on Ubuntu I think? TODO: Recheck that)
-    elif [ -r /etc/rc.status ]; then
-        source /etc/rc.status
-        [ zz`type -t rc_reset 2>/dev/null` == "zzfunction" ] && dtype="suse"
-
-    # Then test against Debian, Ubuntu and friends
-    elif [ -r /lib/lsb/init-functions ]; then
-        source /lib/lsb/init-functions
-        [ zz`type -t log_begin_msg 2>/dev/null` == "zzfunction" ] && dtype="debian"
-
-    # Then test against Gentoo
-    elif [ -r /etc/init.d/functions.sh ]; then
-        source /etc/init.d/functions.sh
-        [ zz`type -t ebegin 2>/dev/null` == "zzfunction" ] && dtype="gentoo"
-
-    # For Slackware we currently just test if /etc/slackware-version exists
-    # and isn't empty (TODO: Find a better way :)
-    elif [ -s /etc/slackware-version ]; then
-        dtype="slackware"
-    fi
-    echo $dtype
-}
+echo "${distroname}"
